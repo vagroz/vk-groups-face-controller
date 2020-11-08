fb98f9531c5df036b56508f8ef32dacaf546dd6b
Ivan Malyshev <vanya-grozny@yandex.ru>
2020-11-08 18:46:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# vk-groups-face-controller
---
+++ src/settings.py
@@ -0,0 +1,7 @@
+community_access_token = "000"
+my_group_id = 000
+my_app_access_token = "000"
+my_id = 000
+my_app_id = 000
+enemy_community_id = 000
+enemy_user_id = 000
